ed0663d59cdd118c5732e8746627afdd8b749851
Dan Martin <thedanmartin@gmail.com>
2015-04-10 12:38:59 -0500

    attempting to create migration between Supporters and Trips

--- app/controllers/supporters_trips_controller.rb
+++ app/controllers/supporters_trips_controller.rb
@@ -1,2 +1,21 @@
 class SupportersTripsController < ApplicationController
+
+  def change
+    create_table :supporters do |t|
+      t.string :name
+      t.timestamps null: false
+    end
+
+    create_table :trips do |t|
+      t.string :location
+      t.timestamps null: false
+    end
+
+    create_table :supporters_trips, id: false do |t|
+      t.belongs_to :supporter, index: true
+      t.belongs_to :trip, index: true
+    end
+  end
+end
+
 end
